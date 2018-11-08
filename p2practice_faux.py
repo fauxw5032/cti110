42a80a68549c270f39761bd056934a5035039732
fauxw5032 <44269454+fauxw5032@users.noreply.github.com>
2018-11-08 11:41:22 -0500

    Add files via upload

---
+++ p2practice_faux.py
@@ -0,0 +1,15 @@
+# CTI 110
+# p2practice
+# walter faux
+# 11/8/2018
+
+
+# ask user their age
+#tell them how old their are next year
+#header
+#get input
+number = int(input("how old are you "))
+#calculate the answer
+answer= number + 2
+#print output
+print("your result is ",answer)
